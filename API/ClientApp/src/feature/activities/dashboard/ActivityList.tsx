b4e199cbbb2da3ad96c986abe04b92f3cbb00781
avichhetri <avichhetri>
2021-10-16 15:15:10 +0545

    activity list to activitylist item

--- API/ClientApp/src/feature/activities/dashboard/ActivityList.tsx
+++ API/ClientApp/src/feature/activities/dashboard/ActivityList.tsx
@@ -1,46 +1,20 @@
 ﻿import { observer } from 'mobx-react-lite';
-import React, { SyntheticEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Item, Label, Segment } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/Activity';
+import React from 'react';
+import { Item, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
+import { ActivityListItem } from './ActivityListItem';
 
 
 export const ActivityList = observer(() => {
-    const [target, setTarget] = useState('');
     const { activityStore } = useStore();
-    const { activitiesByDate : activities, deleteActivity, loading :submitting } = activityStore;
+    const { activitiesByDate : activities} = activityStore;
 
-    const handleActivityDelete = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
-        setTarget(event.currentTarget.name);
-        deleteActivity(id);
-    }
+   
     const DisplayList = () => {
         return (
             <>
                 {activities.map(activity =>
-
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                {activity.description}
-                            </Item.Description>
-                            <Item.Extra>
-                                <Label basic content={activity.category}></Label>
-                                <Button floated='right' color='red'
-                                    loading={submitting && target === activity.id}
-                                    name={activity.id}
-                                    onClick={(e) => handleActivityDelete(e, activity.id)}
-                                >Delete</Button>
-                                <Button floated='right'
-                                    color='blue' as={Link} to={`/activities/${activity.id}`}
-                                >View</Button>
-                                
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
+                    <ActivityListItem key={activity.id} activity={activity} />
                 )}
             </>
         )
---
+++ API/ClientApp/src/feature/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,47 @@
+﻿import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Item, Label } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/Activity';
+import { useStore } from '../../../app/stores/store';
+
+interface Props {
+    activity: IActivity;
+}
+
+export const ActivityListItem = ({ activity }: Props) => {
+    const [target, setTarget] = useState('');
+    const { activityStore } = useStore();
+    const { activitiesByDate: activities, deleteActivity, loading: submitting } = activityStore;
+
+    const handleActivityDelete = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+        setTarget(event.currentTarget.name);
+        deleteActivity(id);
+    }
+
+    return (
+        <>
+            <Item key={activity.id}>
+                <Item.Content>
+                    <Item.Header as='a'>{activity.title}</Item.Header>
+                    <Item.Meta>{activity.date}</Item.Meta>
+                    <Item.Description>
+                        {activity.description}
+                    </Item.Description>
+                    <Item.Extra>
+                        <Label basic content={activity.category}></Label>
+                        <Button floated='right' color='red'
+                            loading={submitting && target === activity.id}
+                            name={activity.id}
+                            onClick={(e) => handleActivityDelete(e, activity.id)}
+                        >Delete</Button>
+                        <Button floated='right'
+                            color='blue' as={Link} to={`/activities/${activity.id}`}
+                        >View</Button>
+
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+        </>
+    )
+}
