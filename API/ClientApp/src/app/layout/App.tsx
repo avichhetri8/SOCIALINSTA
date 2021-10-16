b7505bd3fce096f5910e5a9ed27f503a05730fdb
avichhetri <avichhetri>
2021-10-16 11:04:49 +0545

    fixed key route

--- API/ClientApp/src/app/layout/App.tsx
+++ API/ClientApp/src/app/layout/App.tsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Button, Container, Header, List } from 'semantic-ui-react';
 import { NavBar } from './NavBar';
 import { ActivityDashboard } from '../../feature/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import { HomePage } from '../../feature/home/HomePage';
 import { ActivityForm } from '../../feature/activities/form/ActivityForm';
 import { ActivityDetail } from '../../feature/activities/details/ActivityDetail';
 
 
 const App = () => {
+    const location = useLocation();
 
     return (
         <>
-            <NavBar />
-            <Container style={{ marginTop: '5em' }}>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/activities/" component={ActivityDashboard} />
-                <Route path="/activities/:id" component={ActivityDetail} />
-                <Route path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            <Route exact path='/' component={HomePage} />
+            <Route
+                path={'/(.+)'}
+                render={() => (
 
-            </Container>
+                    <>
+                        <NavBar />
+                        <Container style={{ marginTop: '5em' }}>
+                            <Route exact path="/activities/" component={ActivityDashboard} />
+                            <Route path="/activities/:id" component={ActivityDetail} />
+                            <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+
+                        </Container>
+                    </>
+                )}
+            />
         </>
     );
 }
